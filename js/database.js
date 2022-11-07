51ed154f229897638ec1374c67087717d106a36d
Austin Moore <austin1moore@gmail.com>
2022-11-07 13:08:13 -0600

    starting js with database

---
+++ js/database.js
@@ -0,0 +1,48 @@
+import {openDB} from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+
+export const putDb = async (content) => {
+  console.log('Post to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ id:1, text: content});
+
+  // Get confirmation of the request.
+  const result = await request;
+
+  console.log('🚀 - data saved to the database', result);
+}
+
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jatesDb = await openDB('jate', 1);
+  const tx = jatesDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+
+}
+initdb();
